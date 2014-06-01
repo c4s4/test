8e3e318d51703c90693b5afd6243192e13da7ae9
Michel Casabianca <casa@sweetohm.net>
2014-06-02 00:52:25 +0200

    test

--- test.go
+++ test.go
@@ -1,6 +1,18 @@
 package main
 
-func main() {
-    println("Hello World!")
-}
+import "github.com/russross/blackfriday"
 
+const (
+    input = `# This is a test
+
+A blank line.
+
+## Second level title
+
+Some *random* text, **nothing** important!`
+)
+
+func main() {
+    output := blackfriday.MarkdownBasic(input)
+    println(output)
+}

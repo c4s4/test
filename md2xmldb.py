3f7956fb87651cbe0be84ee558b511198daeee28
Michel Casabianca <michel.casabianca@gmail.com>
2014-06-14 15:57:27 +0200

    Added comments

--- md2xmldb.py
+++ md2xmldb.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 # encoding: UTF-8
+# 
+# Script to generate beedoc files, with metadata, from sqlite database of the
+# sweetblog project.
 
+import codecs
 import sqlite3
 
-QUERY = "SELECT title, text FROM entries WHERE id=%s"
-TEMPLATE = '''# id:       %(index)s
+QUERY = "SELECT date, title, text FROM entries WHERE id=%s"
+TEMPLATE = u'''# id:       %(index)s
+# date:     %(date)s
 # title:    %(title)s
 # author:   Michel Casabianca
 # email:    michel.casabianca@gmail.com
@@ -15,17 +20,22 @@ TEMPLATE = '''# id:       %(index)s
 connection = sqlite3.connect('sweetblog.db')
 cursor = connection.cursor()
 number = cursor.execute("SELECT count(*) FROM entries").fetchone()[0]
-for index in range(number):
+for index in range(number+1):
     result = cursor.execute(QUERY % index).fetchone()
     if result:
-        title = result[0].strip()
-        text = result[1].strip()
+        date = result[0].strip()
+        title = result[1].strip()
+        text = result[2].strip().replace('\r\n', '\n')
         params = {
+            'date':  date,
             'index': index,
             'title': title,
             'text':  text,
         }
-        open("/tmp/%s.bd" % index, 'wb').write(TEMPLATE % params) 
+        document = TEMPLATE % params
+        stream = codecs.open("bd/%s.bd" % index, 'wb', encoding='UTF-8')
+        stream.write(document) 
         print index, title
+        stream.close()
 connection.close()
 

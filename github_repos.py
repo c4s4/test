bf08d932ac9707ff8522a75313515f73e7e5012f
Michel Casabianca <casa@sweetohm.net>
2014-06-28 12:40:42 +0200

    Added user-agebt geader

--- github_repos.py
+++ github_repos.py
@@ -7,9 +7,12 @@ import http1
 
 USER = 'c4s4'
 URL = "https://api.github.com/users/%s/repos"
+HEADERS = {
+    'User-Agent': 'casa',
+}
 
 def main(user):
-    response = http1.get(URL % user)
+    response = http1.get(URL % user, headers=HEADERS)
     if response.status != 200:
         print "Error calling API: %s" % response.body
         sys.exit(1)

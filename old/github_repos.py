c043d916ed6716d615547ba90772c62fad0dca47
Michel Casabianca <casa@sweetohm.net>
2014-07-22 21:00:38 +0200

    Moved to old

--- old/github_repos.py
+++ old/github_repos.py
@@ -5,6 +5,10 @@ import sys
 import json
 import http1
 
+HELP = '''Print project for a given Github user.
+Usage: githut_repos [-h] user
+-h     to print this help page
+user   the github user to print projects for'''
 USER = 'c4s4'
 URL = "https://api.github.com/users/%s/repos"
 HEADERS = {
@@ -19,6 +23,17 @@ def main(user):
     return json.loads(response.body)
 
 if __name__ == '__main__':
-    repos = main(USER)
+    user = USER
+    if len(sys.argv) > 2:
+        print HELP
+        sys.exit(1)
+    if len(sys.argv) > 1:
+        arg = sys.argv[1]
+        if arg == '-h' or arg == '--help':
+            print HELP
+            sys.exit(0)
+        else:
+            user = arg
+    repos = main(user)
     names = sorted([repo['name'] for repo in repos])
     print ' '.join(names)
